40bb22aa2371a55d24df2064d87ce4c1c3d93917
wuxingsong <2774326573@qq.com>
2024-02-26 21:09:55 +0800

    202402262109-my-vim

---
+++ install.sh
@@ -0,0 +1,12 @@
+cd ~/my-vim
+
+echo 'set runtimepath+= /home/wuxingsong/my-vim
+
+source ~/my-vim/vimrcs/basic.vim
+source ~/my-vim/vimrcs/plugins.vim
+source ~/my-vim/vimrcs/plugins_config.vim
+'
+> ~/.vimrc
+
+echo "installed the ultimate Vim configuration successfully! Enjoy :-)"
+
